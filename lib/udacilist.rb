20b7e5363020ead8465b0aea4684af58fb604b59
daminal <drummersanfran@yahoo.com>
2016-08-12 19:46:04 -0400

    errors.rb: added required error classes. udacilist.rb: called first error.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -6,9 +6,19 @@ class UdaciList
   end
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    begin
+      if type == "todo"
+        @items.push TodoItem.new(description, options)
+      elsif type == "event"
+        @items.push EventItem.new(description, options) 
+      elsif type == "link"
+        @items.push LinkItem.new(description, options)
+      else 
+        raise UdaciListErrors::InvalidItemType.new, 'Invalid list item type.'
+      end
+    end
+    rescue StandardError => e
+      puts "#{e.class}: #{e.message}"
   end
   def delete(index)
     @items.delete_at(index - 1)
